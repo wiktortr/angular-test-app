30eb650caf6b7531fffbe5b82621da28dc5cc250
Wiktor Trybulec <wiktortr9@gmail.com>
2021-09-08 14:52:21 +0200

    Add ci pipline

--- karma.conf.js
+++ karma.conf.js
@@ -12,6 +12,12 @@ module.exports = function (config) {
       require('karma-coverage'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
+    customLaunchers: {
+      ChromeHeadlessCI: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
+    },
     client: {
       jasmine: {
         // you can add configuration options for Jasmine here
